3c4dc05000ebf971d3a70bb5950fb662673a15f8
admin <12345678>
2024-04-09 21:48:22 +0300

    KS-1-2. Number of staff

---
+++ src/main/kotlin/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package org.example
+
+
+fun main() {
+
+    var orderCount : Int = 75
+    val thanksForOrderText : String = "Спасибо, что выбрали наш магазин!"
+
+    println(orderCount)
+    println(thanksForOrderText)
+
+    var numberOfStaff = 2000
+//    println(numberOfStaff)
+
+    numberOfStaff = numberOfStaff - 1
+    println(numberOfStaff)
+
+}
