2f111548253e0183205dd65d98061549cf3d169b
admin <12345678>
2024-04-09 21:58:56 +0300

    KS-1-3. Gagarin's flight and landing

---
+++ src/main/kotlin/lessson!_1_task_3.kt
@@ -0,0 +1,23 @@
+package org.example
+
+fun main() {
+
+    val year = 1961
+
+    var hour : String
+    var minute : String
+
+    hour = "09"
+    minute = "07"
+
+    println("""$year 
+        |$hour
+        |$minute
+    """.trimMargin(), )
+
+    hour = "10"
+    minute = "55"
+
+    println("$hour:$minute")
+
+}
