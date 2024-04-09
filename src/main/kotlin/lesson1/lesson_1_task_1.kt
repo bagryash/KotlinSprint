6357bbd066c3a70bf054743c3a07099832be37e7
admin <12345678>
2024-04-09 21:50:35 +0300

    KS-1-1. Error fex

--- src/main/kotlin/lesson1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson1/lesson_1_task_1.kt
@@ -3,6 +3,6 @@ package org.example.lesson1
 fun main() {
 
     var orderCount : Int = 75
-    val thanksForOrderText  = "Спасибо, что выбрали наш магазин!"
+    val thanksForOrderText : String  = "Спасибо, что выбрали наш магазин!"
 
 }
